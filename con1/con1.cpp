085e95d24ffa1d31c48d6bb440475205efdc63ee
kamokar <kamokar@wp.pl>
2016-01-07 17:15:51 +0100

    some colors in console

--- con1/con1.cpp
+++ con1/con1.cpp
@@ -5,25 +5,35 @@ using namespace std;
 #define PI 3.14
 HWND myconsole = GetConsoleWindow();
 HDC mydc = GetDC(myconsole);
-COLORREF COLOR = RGB(0, 255, 0);
+COLORREF kolor = RGB(0, 255, 0);
 
 class punkt 
 {
 public:
 	int x, y;
-	void rysuj(int x, int y)
+	punkt(int xx, int yy) :x(xx), y(yy) { rysuj(); };
+	void rysuj()
 	{
-		SetPixel(mydc, x*20, 20*y+100, COLOR);
+		SetPixel(mydc, x, y, kolor);
 	};
 };
 
 int main()
 {
-	for (int x = 0; x < 28; x++)
-	{
-		punkt;
-		cout << x << "\r";
-	}
+		for (int y = 0; y < 400;y++)
+			for (int x = 0; x < 1000; x++)
+			{
+				kolor = RGB(x, y , rand());
+				punkt(x, y);
+			}
+
+
+
+
+
+
+
+
 
 	system("pause");
 	ReleaseDC(myconsole, mydc);
